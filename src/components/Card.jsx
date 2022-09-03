1a54719c7d606f8bd60763b6b637a6dab389a8d6
Pacifist <desmondmorales92@gmail.com>
2022-09-03 16:44:39 +0300

    fixing issue when empty props render in DOM

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 class Card extends React.Component {
     render() {
-        return (<div className="card">
-            <div className="card-body">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">Some quick example text to build on the card</p>
-                <button type="button" className="btn btn-primary">Go somewhere</button>
+        return(
+            <div className="card">
+                <div className="card-body">
+                    {this.props.title ? <h4 className="card-title">{this.props.title || null}</h4> : null}
+                    {this.props.text ? <p className="card-text">{this.props.text || null}</p> : null}
+                </div>
             </div>
-        </div>)
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Card from "./components/Card";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Card />
+        <Card title="First title" />
+        <Card text="Second text"/>
+        <Card title="Second title" text="Second text"/>
     </React.StrictMode>
 );
